442f4fd129d4f0e48fb6838cf71ca3c75bb089e2
Jaewon Bae <jaey.pro@gmail.com>
2023-02-24 00:58:54 -0500

    refactor components with prop drilling

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,15 +3,14 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-  const interviewerItems = props.interviewers.map((interviewerItem) => {
+  const interviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
-        key={interviewerItem.id}
-        id={interviewerItem.id}
-        name={interviewerItem.name}
-        avatar={interviewerItem.avatar}
-        selected={props.interviewer === interviewerItem.id}
-        setInterviewer={(event) => props.setInterviewer(interviewerItem.id)}
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
       />
     );
   });
@@ -19,7 +18,7 @@ export default function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{interviewerItems}</ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 }
