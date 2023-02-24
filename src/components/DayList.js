da8c4f5305dc53b8a535f8921c2fcdcf42e23f4e
Jaewon Bae <jaey.pro@gmail.com>
2023-02-23 21:41:55 -0500

    add components into application

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,25 +3,6 @@ import classNames from "classnames";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  //   const testClass = classNames("day-list__item", {
-  //     "day-list__item--selected": props.selected,
-  //     "day-list__item--full": !props.spots,
-  //   });
-
-  //   const formatSpots = (spots) => {
-  //     if (!spots) {
-  //       return "no spots remaining";
-  //     }
-
-  //     if (spots === 1) {
-  //       return `${spots} spot remaining`;
-  //     }
-
-  //     return `${spots} spots remaining`;
-  //   };
-
-  //   const spotsMsg = formatSpots(props.spots);
-
   const dayItems = props.days.map((dayItem) => {
     return (
       <DayListItem
