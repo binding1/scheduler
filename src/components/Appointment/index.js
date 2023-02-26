3180452d9b924b3ad69d3e13784381ad30e63fb3
Jaewon Bae <jaey.pro@gmail.com>
2023-02-26 02:15:14 -0500

    insert header, show and empty components for Appointment index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,21 @@ import classNames from "classnames";
 import "./styles.scss";
 
 import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  //   const buttonClass = classNames("button", {
-  //     "button--confirm": props.confirm,
-  //     "button--danger": props.danger,
-  //   });
-
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
