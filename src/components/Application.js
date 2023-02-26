10beb5872d79568e68e6fd9497633c5ccb227190
Jaewon Bae <jaey.pro@gmail.com>
2023-02-26 18:07:44 -0500

    managed states in application

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,13 +45,21 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  });
+
+  const setDay = (day) => setState({ ...state, day });
+
+  const setDays = (days) => {
+    setState((prev) => ({ ...prev, days }));
+  };
 
   useEffect(() => {
-    Axios.get("http://localhost:8001/api/days").then((res) => {
-      setDays([...res.data]);
-    });
+    const dayURL = "http://localhost:8001/api/days";
+    Axios.get(dayURL).then((response) => setDays([...response.data]));
   }, []);
 
   const appointment = Object.values(appointments).map((appointment) => {
@@ -68,7 +76,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
