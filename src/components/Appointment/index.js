1bc586275a331f13a8c19738a7b2725a17f70895
Jaewon Bae <jaey.pro@gmail.com>
2023-02-25 23:10:22 -0500

    created appointment folder

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classNames from "classnames";
+import "./styles.scss";
+
+export default function Appointment(props) {
+  //   const buttonClass = classNames("button", {
+  //     "button--confirm": props.confirm,
+  //     "button--danger": props.danger,
+  //   });
+
+  return <article className="appointment"></article>;
+}
